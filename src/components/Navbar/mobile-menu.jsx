86a544ce6ab67755c25a9995c350f578914c7f42
Edgar Damasceno <edgar@nupix.app>
2024-04-04 13:02:38 -0400

    fix[mobile navigation]: fix z order

--- src/components/Navbar/mobile-menu.jsx
+++ src/components/Navbar/mobile-menu.jsx
@@ -2,7 +2,7 @@ import { BrandLink, DeveloperCredit, MobileToggle, NavItem } from './';
 
 export const MobileMenu = ({ isNavOpen, toggleNav, menuItems }) => {
     return (
-        <div className={isNavOpen ? 'flex flex-col fixed md:hidden left-0 top-0 w-full h-full bg-slate-100 text-slate-600 ease-in-out duration-500' : 'ease-in-out duration-500 fixed top-0 bottom-0 left-[-100%]'}>
+        <div className={isNavOpen ? 'z-50 flex flex-col fixed md:hidden left-0 top-0 w-full h-full bg-slate-100 text-slate-600 ease-in-out duration-500' : 'ease-in-out duration-500 fixed top-0 bottom-0 left-[-100%]'}>
             <div className='px-4 h-12 border-b-[1px] border-b-slate-200px-4 w-full flex flex-row items-center justify-between'>
                 <BrandLink />
                 <MobileToggle isNavOpen={isNavOpen} toggleNav={toggleNav} />
