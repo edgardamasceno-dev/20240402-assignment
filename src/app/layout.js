5da9245796aebdef9c25367d6fbaef8fe87d2825
Edgar Damasceno <edgar@nupix.app>
2024-04-05 18:19:52 -0400

    refact: fine tune

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,12 @@ import { Navbar } from "@/components/Navbar";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+  subsets: ["latin"],
+  weights: ["300", "400", "600"],
+  styles: ["normal"],
+  display: "swap",
+});
 
 export const metadata = {
   title: "Camisetei Store",
