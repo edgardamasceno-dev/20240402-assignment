933b68c25fe5510c6d1632c8e5ee903993c229cd
Edgar Damasceno <edgar@nupix.app>
2024-04-05 23:13:40 -0400

    feat[page]: add blank contact and about pages

---
+++ src/app/contact/page.jsx
@@ -0,0 +1,9 @@
+const Page = () => {
+    return (
+        <section className='flex flex-col items-center justify-between max-w-[400px] mx-auto flex-1 px-4 py-2 md:max-w-[md]'>
+
+        </section>
+    )
+}
+
+export default Page;
